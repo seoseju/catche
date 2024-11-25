26cf1cb3b1cdea0e5d7ca73514485e6be9bc1367
tykim0O0 <emily9943@naver.com>
2024-11-25 22:09:34 +0900

    초기 파일 업로드

---
+++ cache.c
@@ -0,0 +1,126 @@
+/*
+ * cache.c
+ *
+ * 20493-01 Computer Architecture
+ * Term Project on Implementation of Cache Mechanism
+ *
+ * Skeleton Code Prepared by Prof. HyungJune Lee
+ * Nov 17, 2024
+ *
+ */
+
+
+#include <stdio.h>
+#include <string.h>
+#include "cache_impl.h"
+
+extern int num_cache_hits;
+extern int num_cache_misses;
+
+extern int num_bytes;
+extern int num_access_cycles;
+
+extern int global_timestamp;
+
+cache_entry_t cache_array[CACHE_SET_SIZE][DEFAULT_CACHE_ASSOC];
+int memory_array[DEFAULT_MEMORY_SIZE_WORD];
+
+
+/* DO NOT CHANGE THE FOLLOWING FUNCTION */
+void init_memory_content() {
+    unsigned char sample_upward[16] = {0x001, 0x012, 0x023, 0x034, 0x045, 0x056, 0x067, 0x078, 0x089, 0x09a, 0x0ab, 0x0bc, 0x0cd, 0x0de, 0x0ef};
+    unsigned char sample_downward[16] = {0x0fe, 0x0ed, 0x0dc, 0x0cb, 0x0ba, 0x0a9, 0x098, 0x087, 0x076, 0x065, 0x054, 0x043, 0x032, 0x021, 0x010};
+    int index, i=0, j=1, gap = 1;
+    
+    for (index=0; index < DEFAULT_MEMORY_SIZE_WORD; index++) {
+        memory_array[index] = (sample_upward[i] << 24) | (sample_upward[j] << 16) | (sample_downward[i] << 8) | (sample_downward[j]);
+        if (++i >= 16)
+            i = 0;
+        if (++j >= 16)
+            j = 0;
+        
+        if (i == 0 && j == i+gap)
+            j = i + (++gap);
+            
+        printf("mem[%d] = %#x\n", index, memory_array[index]);
+    }
+}   
+
+/* DO NOT CHANGE THE FOLLOWING FUNCTION */
+void init_cache_content() {
+    int i, j;
+    
+    for (i=0; i<CACHE_SET_SIZE; i++) {
+        for (j=0; j < DEFAULT_CACHE_ASSOC; j++) {
+            cache_entry_t *pEntry = &cache_array[i][j];
+            pEntry->valid = 0;
+            pEntry->tag = -1;
+            pEntry->timestamp = 0;
+        }
+    }
+}
+
+/* DO NOT CHANGE THE FOLLOWING FUNCTION */
+/* This function is a utility function to print all the cache entries. It will be useful for your debugging */
+void print_cache_entries() {
+    int i, j, k;
+    
+    for (i=0; i<CACHE_SET_SIZE; i++) {
+        printf("[Set %d] ", i);
+        for (j=0; j <DEFAULT_CACHE_ASSOC; j++) {
+            cache_entry_t *pEntry = &cache_array[i][j];
+            printf("V: %d Tag: %#x Time: %d Data: ", pEntry->valid, pEntry->tag, pEntry->timestamp);
+            for (k=0; k<DEFAULT_CACHE_BLOCK_SIZE_BYTE; k++) {
+                printf("%#x(%d) ", pEntry->data[k], k);
+            }
+            printf("\t");
+        }
+        printf("\n");
+    }
+}
+
+int check_cache_data_hit(void *addr, char type) {
+
+    /* Fill out here */
+
+
+
+
+
+
+
+    /* Return the data */
+    return 0;    
+}
+
+int find_entry_index_in_set(int cache_index) {
+    int entry_index;
+
+    /* Check if there exists any empty cache space by checking 'valid' */
+
+
+    /* Otherwise, search over all entries to find the least recently used entry by checking 'timestamp' */
+
+
+
+    return entry_index; 
+}
+
+int access_memory(void *addr, char type) {
+    
+    /* Fetch the data from the main memory and copy them to the cache */
+    /* void *addr: addr is byte address, whereas your main memory address is word address due to 'int memory_array[]' */
+
+    /* You need to invoke find_entry_index_in_set() for copying to the cache */
+
+
+
+
+    /* Return the accessed data with a suitable type */    
+
+
+
+
+
+    return 0;
+}
---
+++ main.c
@@ -0,0 +1,69 @@
+/*
+ * main.c
+ *
+ * 20493-01 Computer Architecture
+ * Term Project on Implementation of Cache Mechanism
+ *
+ * Skeleton Code Prepared by Prof. HyungJune Lee
+ * Nov 17, 2024
+ *
+ */
+
+#include <stdio.h>
+#include "cache_impl.h"
+
+int num_cache_hits = 0;
+int num_cache_misses = 0;
+
+int num_bytes = 0;
+int num_access_cycles = 0;
+
+int global_timestamp = 0;
+
+int retrieve_data(void *addr, char data_type) {
+    int value_returned = -1; /* accessed data */
+
+    /* Invoke check_cache_data_hit() */
+
+
+    
+    /* In case of the cache miss event, access the main memory by invoking access_memory() */
+
+
+
+    return value_returned;    
+}
+
+int main(void) {
+    FILE *ifp = NULL, *ofp = NULL;
+    unsigned long int access_addr; /* byte address (located at 1st column) in "access_input.txt" */
+    char access_type; /* 'b'(byte), 'h'(halfword), or 'w'(word) (located at 2nd column) in "access_input.txt" */
+    int accessed_data; /* This is the data that you want to retrieve first from cache, and then from memory */ 
+    
+    init_memory_content();
+    init_cache_content();
+    
+    ifp = fopen("access_input.txt", "r");
+    if (ifp == NULL) {
+        printf("Can't open input file\n");
+        return -1;
+    }
+    ofp = fopen("access_output.txt", "w");
+    if (ofp == NULL) {
+        printf("Can't open output file\n");
+        fclose(ifp);
+        return -1;
+    }
+    
+    /* Fill out here by invoking retrieve_data() */
+
+
+
+
+
+    fclose(ifp);
+    fclose(ofp);
+    
+    print_cache_entries();
+    return 0;
+}
