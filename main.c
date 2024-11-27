6b823431292a8d35c39096beacac48e6023b83a9
seoseju <rosenny1225@naver.com>
2024-11-27 21:54:10 +0900

    fputs제외한 오류 고쳤?어
    char array[][][] 3차원으로 고침: overflow 방지를 위해서 마지막에 [단어의 최대길이]를 넣음..
    accessed_addr 에 atoi함수써서 int->char
    accessed_type 수정
    fputs 일단지움

--- main.c
+++ main.c
@@ -11,6 +11,7 @@
 
 #include <stdio.h>
 #include <string.h> // 내가 했어..
+#include <stdlib.h> //내가 했어..
 #include "cache_impl.h"
 
 int num_cache_hits = 0;
@@ -25,7 +26,7 @@ int retrieve_data(void *addr, char data_type) {
     int value_returned = -1; /* accessed data */
 
     /*check data by invoking check_cache_data_hit()*/
-    printf("===== addr %d type %c ======\n", addr, data_type);
+    printf("===== addr %d type %c ======\n", *(int *)addr, data_type);
     value_returned = check_cache_data_hit(addr, data_type); 
 
     /* In case of the cache miss event, retrieve data from the main memory  by invoking access_memory()*/
@@ -76,41 +77,51 @@ int main(void) {
     // 인풋파일 뜯어서 array 이차원 배열에 저장 
     // array[0][1] : 첫번째 줄의 두번째 단어 == b
 
-    char array[10][10];     //[max_lines][max_word_length]: [최대 줄 수][단어의 최대 길이]
+    char array[10][10][10];     //[max_lines][max_word][max_length]: [최대 줄 수][최대 단어 수][단어의 최대 길이]
     char line[20];          // 한 줄을 저장할 배열 
-    int lineCount =0 ;      //읽은 줄 수 
+    int lineCount =0 ;      //읽은 줄 수
+    int wordCounts[10] = {0};  
 
     while(fgets(line, sizeof(line), ifp) && lineCount < 10){
     
         line[strcspn(line, "\n")] = '\0';  // 줄 끝의 \n제거
 
-        char *word = strtok(line, " "); //첫번째 단어 
+        char *word = strtok(line, " "); //공백으로 단어 분리 
         int wordIndex = 0; 
         while(word != NULL && wordIndex < 2){
             strncpy(array[lineCount][wordIndex], word, 9); 
-            array[lineCount][wordIndex] = '\0'; 
+            array[lineCount][wordIndex][9] = '\0'; 
             word = strtok(NULL, " "); 
             wordIndex ++;
         }
+        wordCounts[lineCount] = wordIndex; 
         lineCount ++; 
     }
 
     // array[i][1] 로 type읽어서 access_type 정하고 array[i][0]으로 access_addr 정하기 
 
+    
+
     for(int i=0; i<10; i++){
 
-        if(array[i][1] == NULL)
+        if(array[i][1][0] == NULL)
             break; 
         else {
-            access_addr = array[i][0];
-            access_type = array[i][1];
+            access_addr = atoi(array[i][0]); // char 형 배열이라서 atoi()함수로 int형전환을 해줌 
+            access_type = array[i][1][0]; // [i][1][0] 이 들어가야 단어의 첫번째 char을 반환한대.. array를 이차원으로 했더니, overflow가 발생할까봐 안된다네..?
             accessed_data = retrieve_data(&access_addr, access_type);
             
+            
             //output파일에 저장
-            fputs(accessed_data, ofp);
+            //fputs()가 char *형을 인풋으로 받는대서... 아예 아웃풋 형식 다 쓴걸 char*에 넣고? fputs를 써야할듯. 
+            //fputs(accessed_data, ofp);
         }
     }
 
+    
+   
+     
+    
     fclose(ifp);
     fclose(ofp);
     
