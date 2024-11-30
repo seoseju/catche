8c3c709c7212fe19b3e7c19dea5e594b17476665
tykim0O0 <emily9943@naver.com>
2024-11-30 17:05:06 +0900

    input 파일 형식 & 읽어오는 행 수 수정

--- main.c
+++ main.c
@@ -77,56 +77,36 @@ int main(void) {
     // 인풋파일 뜯어서 array 이차원 배열에 저장 
     // array[0][1] : 첫번째 줄의 두번째 단어 == b
 
-    char array[10][10][10];     //[max_lines][max_word][max_length]: [최대 줄 수][최대 단어 수][단어의 최대 길이]
-    char line[20];          // 한 줄을 저장할 배열 
-    int lineCount =0 ;      //읽은 줄 수
-    int wordCounts[10] = {0};
-    (void)wordCounts;
+    char line[101];
+    char *array[100];
+    int lineCount=0;
 
-    while(fgets(line, sizeof(line), ifp) && lineCount < 10){
-    
-        line[strcspn(line, "\n")] = '\0';  // 줄 끝의 \n제거
-
-        char *word = strtok(line, " "); //공백으로 단어 분리 
-        int wordIndex = 0; 
-        while(word != NULL && wordIndex < 2){
-           
-            strncpy(array[lineCount][wordIndex], word, 9); 
-            array[lineCount][wordIndex][9] = '\0'; 
-            word = strtok(NULL, " "); 
-            wordIndex ++;
-
-             if(array[lineCount][0][0] == NULL){
-                break; 
-            }
+    while(fgets(line, sizeof(line), ifp)!=NULL && lineCount < 100){
+        array[lineCount] = malloc(sizeof(char)*101);
+        char *token = strtok(line, " \n");
+        int tokenIndex = 0;
+        while(token!=NULL&&tokenIndex<2){
+            if(tokenIndex==0)
+                strncpy(array[lineCount], token, 101);
+            else if(tokenIndex==1)
+                array[lineCount][100] = token[0];
+            token=strtok(NULL, " \n");
+            tokenIndex++;
         }
-        wordCounts[lineCount] = wordIndex; 
-        lineCount ++; 
+        lineCount++;
     }
 
     // array[i][1] 로 type읽어서 access_type 정하고 array[i][0]으로 access_addr 정하기 
 
-    char s[1000]; 
-    
+    char s[1000] = "";
     fprintf(ofp,"[Accessed Data]\n");
-    for(int i=0; i<10; i++){
 
-        if(array[i][1][0] == '\0')
-            break; 
-        else {
-            access_addr = (unsigned long)atoi(array[i][0]); // char 형 배열이라서 atoi()함수로 int형전환을 해줌 
-            access_type = array[i][1][0]; // [i][1][0] 이 들어가야 단어의 첫번째 char을 반환한대.. array를 이차원으로 했더니, overflow가 발생할까봐 안된다네..?
-            accessed_data = retrieve_data(&access_addr, access_type);
-            print_cache_entries();
-            snprintf(s+strlen(s) , sizeof(s)-strlen(s),"%d\t%c\t%#x\n",access_addr,access_type,accessed_data); 
-        
-            printf("잘됐어요 : \n%s",s); 
-            //디버깅용 프린트
-            printf("%#x\n", accessed_data);
-            
-
-            
-        }
+    for(int i=0;i<lineCount;i++){
+        access_addr = strtoul(array[i], NULL, 10);
+        access_type = array[i][100];
+        accessed_data = retrieve_data(&access_addr, access_type);
+        print_cache_entries();
+        snprintf(s+strlen(s), sizeof(s) - strlen(s), "%d\t%c\t%#x\n",access_addr,access_type,accessed_data);
     }
 
     // output파일 출력
